f162a3f26e88fcf8fbf258ba72dcf852bb94f5bd
Gionnelly Vielza Durán <80008338+gvielza@users.noreply.github.com>
2024-05-21 00:03:44 +0000

    CRUD sqlite+python

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from base_datos.conexion import Conexion
 '''Implementa un sistema para un supermercado, que  le permita a los usuarios ingresar clientes(nombre, apellido, dni) a la base de datos sqlite3 por consola, editar, eliminar y mostrar todos clientes, de acuerdo a la opción seleccionada.'''
 print("Bienvenido al sistema de supermercado")
 opcion=0
@@ -10,7 +11,34 @@ while(opcion!="5"):
     nombre=input("Ingrese el nombre del cliente: ")
     apellido=input("Ingrese el apellido del cliente: ")
     dni=input("Ingrese el dni del cliente: ")
+    conexion=Conexion("base_datos/supermercado.db")
+    conexion.crear_tabla_cliente()
+    conexion.agregar_cliente(nombre,apellido,dni)
     print("Cliente agregado")
+  if(opcion=="2"):
+    dni=input("Ingrese el dni del cliente a editar: ")
+    
+    conexion=Conexion("base_datos/supermercado.db")
+    
+    clientes=conexion.mostrar_clientes()
+    for cliente in clientes:
+      if(cliente[2]==dni):
+        nombre=input("Ingrese el nuevo nombre del cliente: ")
+        apellido=input("Ingrese el nuevo apellido del cliente: ")
+        conexion.editar_cliente(nombre,apellido,dni)
+        print("Cliente actualizado")
+  if(opcion=="3"):
+    dni=input("Ingrese el dni del cliente a eliminar: ")
+    conexion=Conexion("base_datos/supermercado.db")
+    conexion.eliminar_cliente(dni)
+    print("Cliente eliminado")  
+    
+  if(opcion=="4"):
+    conexion=Conexion("base_datos/supermercado.db")
+    clientes=conexion.mostrar_clientes()
+    for cliente in clientes:
+      print(cliente)
+    
   if(opcion==5):
     print("Saliendo")
 
